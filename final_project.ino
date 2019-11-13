b86b2243eedc081cd3f3fb8b406c7a552099d26e
Colton Sellers <41277998+C-SELLERS@users.noreply.github.com>
2019-11-13 11:37:30 -0800

    Added functionality and definitions for control functions

--- final_project.ino
+++ final_project.ino
@@ -1,146 +1,209 @@
+/*
+CSS 427 UWB
+CODE FOR ARDUINO THERMOSTAT
+DHT SENSOR
+
+*/
+
+
 #include <dht_nonblocking.h>
 #include <IRremote.h>
 #include <LiquidCrystal.h>
+
+//DHT sensor definition
 #define DHT_SENSOR_TYPE DHT_TYPE_11
+
+//Definition for remote functions
+#define POWER   0xFFA25D
+#define FUNC    0xFFE21D
+#define VOLUP   0xFF629D
+#define REWIND  0xFF22DD
+#define PAUSE   0xFF02FD
+#define FASTFOR 0xFFC23D
+#define DOWN    0xFFE01F
+#define VOLDOWN 0xFFA857
+#define UP      0xFF906F
+#define EQ      0xFF9867
+#define ST/REPT 0xFFB04F
+#define ZERO    0xFF6897
+#define ONE     0xFF30CF
+#define TWO     0xFF18E7
+#define THREE   0xFF7A85
+#define FOUR    0xFF10EF
+#define FIVE    0xFF38C7
+#define SIX     0xFF5AA5
+#define SEVEN   0xFF42BD
+#define EIGHT   0xFF4AB5
+#define NINE    0xFF52AD
+#define HOLD    0xFFFFFFFF
+
  
-//                BS  E  D4 D5  D6 D7
+//Initialize LCD Screen
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+//                RS  E  D4 D5  D6 D7
+
+//Initialize DHT Sensor variables
 static const int DHT_SENSOR_PIN = 2;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+//Sensor Variables
 float temperature;
 float humidity;
-int receiver = 3; // Pin 12 on Arduino 
-int relay = 13; // relay pin interface
 
-//THIS IS FOR PROGRAMMING THE REMOTE CONTROLLER.
+//Variables for settings
+int setTemp = 30;
+int setHumid = 45;
+bool manualOn = false; //Manual overide for on
+bool on = false; //off by default
+
+//Relay Pin 13;
+int relay = 13; 
+
 /*
-  case 0xFFA25D: Serial.println("POWER"); break;
-  case 0xFFE21D: Serial.println("FUNC/STOP"); break;
-  case 0xFF629D: Serial.println("VOL+"); break;
-  case 0xFF22DD: Serial.println("FAST BACK");    break;
-  case 0xFF02FD: Serial.println("PAUSE");    break;
-  case 0xFFC23D: Serial.println("FAST FORWARD");   break;
-  case 0xFFE01F: Serial.println("DOWN");    break;
-  case 0xFFA857: Serial.println("VOL-");    break;
-  case 0xFF906F: Serial.println("UP");    break;
-  case 0xFF9867: Serial.println("EQ");    break;
-  case 0xFFB04F: Serial.println("ST/REPT");    break;
-  case 0xFF6897: Serial.println("0");    break;
-  case 0xFF30CF: Serial.println("1");    break;
-  case 0xFF18E7: Serial.println("2");    break;
-  case 0xFF7A85: Serial.println("3");    break;
-  case 0xFF10EF: Serial.println("4");    break;
-  case 0xFF38C7: Serial.println("5");    break;
-  case 0xFF5AA5: Serial.println("6");    break;
-  case 0xFF42BD: Serial.println("7");    break;
-  case 0xFF4AB5: Serial.println("8");    break;
-  case 0xFF52AD: Serial.println("9");    break;
-  case 0xFFFFFFFF: Serial.println(" REPEAT");break;  
+//ONLY FOR SIMULATION TMP VARIABLES
+int sensorPin=0;
+int reading;
+float voltage;
 */
 
-//-----( Declare objects )----- - for remote 
-IRrecv irrecv(receiver);     // create instance of 'irrecv'
-decode_results results;      // create instance of 'decode_results'
+//Initialize the remote control variables
+int receiverPin = 3;
+IRrecv irrecv(receiverPin);
+decode_results results;      
 
-void setup()
-{
+void setup() {
   lcd.begin(16, 2);
   Serial.begin(9600);
   pinMode(relay,OUTPUT);
   irrecv.enableIRIn(); // Start the receiver
 }
-/*
- * Poll for a measurement, keeping the state machine alive.  Returns
- * true if a measurement is available.
- */
-static bool measure_environment( float *temperature, float *humidity )
-{
-  static unsigned long measurement_timestamp = millis( );
 
-  /* Measure once every four seconds. */
-  if( millis( ) - measurement_timestamp > 1000ul )
-  {
-    if( dht_sensor.measure( temperature, humidity ) == true )
-    {
-      measurement_timestamp = millis( );
-      return( true );
+void loop(){
+  
+   
+   
+    //Measure temperature and humidity
+  if( measure_environment( &temperature, &humidity )){
+      Serial.print( "Temperature = " );
+      Serial.print( temperature, 1 );
+      Serial.print( " deg. C, Humidity = " );
+      Serial.print( humidity, 1 );
+      Serial.println( "%" );
+    } else {
+      Serial.println("no reading");
     }
+     
+  /*
+  //ONLY FOR SIMULATION
+  reading = analogRead(sensorPin);  
+  voltage = (reading * 5.0) / 1024.0;
+  temperature = (voltage - 0.5) * 100 ;  
+  */
+  
+  //Print to LCD 
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temp = ");
+    lcd.print(temperature);
+    lcd.print(" C ");
+
+    switch(on){
+      case true:
+        lcd.print("ON");
+        break;
+      case false:
+        lcd.print("OFF");
+        break;
+    }
+    
+    lcd.setCursor(0,1);
+    lcd.print(setTemp);
+    lcd.print(" C");
+  
+    lcd.setCursor(6,1);
+    lcd.print("%");
+    lcd.print(setHumid);
+    
+    
+  
+  //Relay condition to fan
+    if(temperature > setTemp || humidity > setHumid || manualOn) {
+      if(!on){
+        digitalWrite(relay, HIGH); 
+        on = true;
+      }
+    } else {
+      if(on){
+      digitalWrite(relay, LOW);
+      on = false;
+      }
+    }
+  
+  //Remote commands 
+    //TODO: ISR? Seperate method!
+    if (irrecv.decode(&results)){   
+      switch(results.value){
+        case POWER:
+          manualOn = !manualOn;
+          break;
+        
+        case VOLUP:
+          setTemp++;
+          break;
+        
+        case VOLDOWN:
+          setTemp--;
+          break;
+        
+        case ONE: 
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("HELLLLOO");
+          delay(5000);
+          break;
+        
+        case TWO:
+          lcd.clear(); 
+          lcd.setCursor(0,0); 
+          lcd.print("Give Us"); 
+          delay(2000); 
+          lcd.clear(); 
+          lcd.setCursor(0,0); 
+          lcd.print("A Pass");
+          delay(1000);
+          break;       
+        
+        case HOLD: 
+          lcd.clear(); 
+          lcd.setCursor(0,0); 
+          lcd.print("REPEAT");
+          break;
+        
+        default: 
+          lcd.clear(); 
+          lcd.setCursor(0,0); 
+          lcd.print(results.value, HEX);
+          break;
+  }  
+    delay(500);
+    irrecv.resume(); // receive the next value
   }
-  return( false );
+
+  delay(100);
 }
 
 
-void loop()
-{
-  /* Measure temperature and humidity.  If the functions returns
-     true, then a measurement is available. */
-  if( measure_environment( &temperature, &humidity ) == true )
-  {
-    Serial.print( "Temperature = " );
-    Serial.print( temperature, 1 );
-    Serial.print( " deg. C, Humidity = " );
-    Serial.print( humidity, 1 );
-    Serial.println( "%" );
-  }
-// CODE TO CONTROL LCD MONITOR 
-  lcd.setCursor(0, 0);
-  lcd.print("Temp = ");
-  lcd.print(temperature);
-  lcd.print(" C");
+
+
+//Take measurement of environment, if fails to measure return false. 
+static bool measure_environment( float *temperature, float *humidity ) {
+    static unsigned long measurement_timestamp = millis( );
   
-// CODE TO CONTROL LOGIC OF FAN INITIATION
-  if(temperature > 30.0 || humidity > 45)
-  {
-    digitalWrite(relay, HIGH); 
-  }
-  else if(temperature < 30.0 || humidity < 45)
-  {
-    digitalWrite(relay, LOW);
-  }
-  
-/*
-  if (irrecv.decode(&results)) // have we received an IR signal?
-  {   
-    switch(results.value)
-    {
-    case 0xFF30CF: 
-    Serial.println(tempC);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Temp         C  ");
-    lcd.setCursor(6,0);
-    lcd.print(tempC);
-    delay(5000);
-    break;
-    
-    case 0xFF18E7:
-    lcd.clear(); 
-    lcd.setCursor(0,0); 
-    lcd.print("Give Us"); 
-    delay(2000); 
-    lcd.clear(); 
-    lcd.setCursor(0,0); 
-    lcd.print("A Pass");
-    delay(1000);
-    break;    
-    
-    case 0xFF7A85:
-    lcd.clear(); 
-    lcd.setCursor(0,0); 
-    lcd.print("Matt is"); 
-    delay(2000); 
-    lcd.clear(); 
-    lcd.setCursor(0,0); 
-    lcd.print("Sexy");
-    delay(1000);
-    break;    
-    
-    case 0xFFFFFFFF: Serial.println(" REPEAT");break;  
-    default: 
-      Serial.println(" other button : ");
-      Serial.println(results.value);
-     }  
-  delay(500);
-     irrecv.resume(); // receive the next value
-  }*/
+    if( millis( ) - measurement_timestamp > 1000ul ) {
+      if( dht_sensor.measure( temperature, humidity )) {
+          measurement_timestamp = millis( );
+          return(true);
+      }
+    }
+    return(false);
 }
