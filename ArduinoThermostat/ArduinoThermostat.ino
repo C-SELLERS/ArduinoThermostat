1f0696cc39cb24a9d15e0375733e06d7f12490b8
Christian M. Rahmel <35943405+OmeganKryist@users.noreply.github.com>
2019-12-02 14:28:39 -0800

    Update ArduinoThermostat.ino

--- ArduinoThermostat/ArduinoThermostat.ino
+++ ArduinoThermostat/ArduinoThermostat.ino
@@ -12,31 +12,30 @@ DHT SENSOR
 
 
 //Definition for remote functions
-#define POWER   0xFFA25D
-#define FUNC    0xFFE21D
-#define VOLUP   0xFF629D
-#define REWIND  0xFF22DD
-#define PAUSE   0xFF02FD
-#define FASTFOR 0xFFC23D
-#define DOWN    0xFFE01F
-#define VOLDOWN 0xFFA857
-#define UP      0xFF906F
-#define EQ      0xFF9867
-#define STREPT  0xFFB04F
-#define ZERO    0xFF6897
-#define ONE     0xFF30CF
-#define TWO     0xFF18E7
-#define THREE   0xFF7A85
-#define FOUR    0xFF10EF
-#define FIVE    0xFF38C7
-#define SIX     0xFF5AA5
-#define SEVEN   0xFF42BD
-#define EIGHT   0xFF4AB5
-#define NINE    0xFF52AD
-#define HOLD    0xFFFFFFFF
+#define POWER         0xFFA25D
+#define FUNC          0xFFE21D
+#define VOLUP         0xFF629D
+#define REWIND        0xFF22DD
+#define PAUSE         0xFF02FD
+#define FASTFOR       0xFFC23D
+#define DOWN          0xFFE01F
+#define VOLDOWN       0xFFA857
+#define UP            0xFF906F
+#define EQ            0xFF9867
+#define STREPT        0xFFB04F
+#define ZERO          0xFF6897
+#define ONE           0xFF30CF
+#define TWO           0xFF18E7
+#define THREE         0xFF7A85
+#define FOUR          0xFF10EF
+#define FIVE          0xFF38C7
+#define SIX           0xFF5AA5
+#define SEVEN         0xFF42BD
+#define EIGHT         0xFF4AB5
+#define NINE          0xFF52AD
+#define HOLD          0xFFFFFFFF
 
 //Definition for volume button mapping
-#define CONTROLOFF   -1
 #define CONTROLTEMP   0
 #define CONTROLHUMID  1
 #define CONTROLFAN    2
@@ -44,16 +43,17 @@ DHT SENSOR
 // Definition for fan range
 #define MAXRES        150
 #define MINRES        85
+#define OFF           0
 #define RESSTEP       32
 
 //Definition for operating modes
-#define MANUAL  true
-#define AUTO    false
+#define MANUAL        true
+#define AUTO          false
 
 //These are the pin outs to the arduino from the H-Bridge IC 
-#define ENABLE 5
-#define DIRA   6
-#define DIRB   4
+#define ENABLE        5
+#define DIRA          6
+#define DIRB          4
  
 //Initialize LCD Screen
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
@@ -78,7 +78,6 @@ int curTemp = 0;
 int curHum = 0;
 int resolution = (MAXRES + MINRES) / 2;
 bool changes = true; //true for initial bootup
-bool manualOn = false; //Manual overide for on
 bool on = false; //Current signal to fan
 bool mode = MANUAL; //System's current mode
 int volButtonMapping = CONTROLTEMP; //Indicate which threshold VOL buttons will change
@@ -121,22 +120,22 @@ void loop(){
         if(temperature > setTemp + 6 || humidity > setHumid + 12)
         {
           resolution = MAXRES;
-          turnOn();
         }
         else if(temperature > setTemp + 3 || humidity > setHumid + 6)
         {
           resolution = (MAXRES + MINRES) / 2;
-          turnOn();
         }
         else if(temperature > setTemp || humidity > setHumid)
         {
           resolution = MINRES;
-          turnOn();
         }
-        else turnOff();
+        else 
+        {
+          resolution = OFF;
+        }
+        turn();
       } else if(mode == MANUAL){
-        if(manualOn) turnOn();
-        else turnOff();
+        turn();
       }
 
         updateScreen();
@@ -184,18 +183,10 @@ void printThresholdChange() {
 
 //YOU CAN CONTROL THE SPEED OF THE FAN BY MANIPULATING THE 2ND PARAMETER IN ANALOG WRITE
 //THIS POWER/VALUE HAS A RANGE FROM 0-255 (255 FULL POWER 0 OFF); 
-void turnOn(){
-  if(!on){
-      analogWrite(ENABLE,resolution);
-      on = true;
-    }
-}
-
-void turnOff(){
-  if(on){
-    analogWrite(ENABLE, 0); 
-    on = false;
-  }
+void turn()
+{
+   analogWrite(ENABLE, resolution);
+   on = true;
 }
 
 //Void for changing screen if changes occur
@@ -221,7 +212,7 @@ void updateScreen(){
     }
 
     lcd.setCursor(0,1);
-  	lcd.print("SET");
+  	 lcd.print("SET");
 
     lcd.setCursor(4, 1);
     lcd.print(setTemp);
@@ -252,44 +243,30 @@ void ControlFunction(decode_results results){
 
         //Switch the mode that the device is in
         case POWER:
-          if(mode == MANUAL) {
-            //Manual off goes to manual on
-            if(!manualOn) {
-              manualOn = true;
+          if(mode == AUTO) {
+              mode = MANUAL;
               // In manual mode, volume buttons control fan strength
               volButtonMapping = CONTROLFAN;
               resolution = (MINRES + MAXRES) / 2;
               lcd.clear();
               lcd.setCursor(0, 0);
-              lcd.print("MANUAL ON");
+              lcd.print("MODE: MANUAL");
               lcd.setCursor(0, 1);
-              lcd.print("CONTROLING FAN");
-            }
-            //Manual on goes to auto
-            else {
+              lcd.print("CONTROLING: FAN");
+           }
+           //Manual goes to auto
+           else {
               mode = AUTO;
               // In auto mode, volume buttons control temp threshold or humidity % threshold
               volButtonMapping = CONTROLTEMP;
               resolution = MINRES;
               lcd.clear();
               lcd.setCursor(0, 0);
-              lcd.print("AUTO");
+              lcd.print("MODE: AUTO");
               lcd.setCursor(0, 1);
-              lcd.print("CONTROLING TEMP");
+              lcd.print("CONTROLING: TEMP");
             }
             delay(1000); //Leave message for a second
-          }
-          //Auto goes to manual off
-          else {
-            mode = MANUAL;
-            manualOn = false;
-            volButtonMapping = CONTROLOFF;
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print("MANUAL OFF");
-            lcd.setCursor(0, 1);
-            lcd.print("CONTROLING OFF");
-            delay(1000); //leave message for a second
           }
           break;
         
@@ -304,7 +281,7 @@ void ControlFunction(decode_results results){
               break;
             case CONTROLFAN:
               resolution = min(MAXRES, resolution + RESSTEP);
-              analogWrite(ENABLE,resolution);
+              turn();
               break;
           }
           //printThresholdChange(); WILL BE REMOVED CURRENT USE SCREEN FOR SHOWING CURRENT SETTTINGS
@@ -321,7 +298,7 @@ void ControlFunction(decode_results results){
               break;
             case CONTROLFAN:
               resolution = max(MINRES, resolution - RESSTEP);
-              analogWrite(ENABLE,resolution);
+              turn();
               break;
           }
           //printThresholdChange(); WILL BE REMOVED CURRENT USE SCREEN FOR SHOWING CURRENT SETTTINGS
